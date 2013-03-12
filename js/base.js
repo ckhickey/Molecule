4b7b1179ae6c303d15518c3073eced5da4d43fe9
Jeremy Rue <jrue@calpress>
2013-03-12 11:40:04 -0700

    Added responsive code to keep videos in the center always by resizing the #container.

--- js/base.js
+++ js/base.js
@@ -1,3 +1,5 @@
+var videoheight, videowidth;
+
 /**
  * This function links to other sections, and loads them into the #container div
  */
@@ -9,6 +11,7 @@ function bindLinksToLoadNewSections(){
       jQuery.ajax({url:href,dataType:'html'}).done(
         function(data){
           jQuery('#container').html(data);
+          centerVideos();
           bindLinksToLoadNewSections();
         });
     }
@@ -16,8 +19,41 @@ function bindLinksToLoadNewSections(){
   });
 }
 
+function centerVideos(){
+	//retrieve the sizes that are set on the video tag
+	videoheight = $('video').eq(0).attr('height');
+	videowidth = $('video').eq(0).attr('width');
+
+	//convert them to numbers, default to 1920x1080
+	videowidth = (videowidth.match(/\%/) || videowidth.match(/auto/)) ? 1920 : videowidth;
+	videoheight = (videoheight.match(/\%/) || videoheight.match(/auto/) || videowidth == 1920) ? 1080 : videoheight;
+	
+	//now that we have the sizes, make it responsive
+	$('video, video object, video img').attr('width', '100%').attr('height', 'auto');
+	
+	//console.log("Window height: " + $(window).height() + ", Window width: " + $(window).width() + ", videowidth: " + videowidth + ", videoheight: " + videoheight);
+	//vertically center the container
+	if($(window).width() * videoheight/videowidth > $(window).height){
+		$('#container').css({'width':Math.floor($(window).height() * videowidth/videoheight), 'height':$(window).height, 'left':($(window).width() - videowidth)/2});
+	} else {
+		$('#container').css({'width':'100%', 'height': Math.floor($(window).width() * videoheight/videowidth), 'top':($(window).height() - Math.floor($(window).width() * videoheight/videowidth))/2});
+	}
+	
+	$(window).on('resize', function(){
+		if($(window).width() * videoheight/videowidth > $(window).height){
+			$('#container').css({'width':Math.floor($(window).height() * videowidth/videoheight), 'height':$(window).height, 'left':($(window).width() - videowidth)/2});
+		} else {
+			$('#container').css({'width':'100%', 'height': Math.floor($(window).width() * videoheight/videowidth), 'top':($(window).height() - Math.floor($(window).width() * videoheight/videowidth))/2});
+		}
+	})
+}
+
+
+
 jQuery(document).ready(function($){
   
+
+  centerVideos();
   //run the function above to setup the links to the different sections
   bindLinksToLoadNewSections();
   
