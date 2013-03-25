d4b17378f4e616f2ce9b05f2e95d48bc71e2c887
C.K. Hickey <ckhickey@me.com>
2013-03-25 10:34:35 -0700

    3/22 Edits
    New SVG menu, revised CSS for text, new transitions

--- js/base.js
+++ js/base.js
@@ -6,17 +6,37 @@ var videoheight, videowidth;
 function bindLinksToLoadNewSections(){
   jQuery('a').on('click', function(e){
     var href = $(this).attr('href') || null;
+	var fade = $(this).data('transition') || null;
+	var speed = $(this).data('transitionspeed') || null;
     if(href.substr(0, 4) != 'http' && href != null){//make sure this isn't an external link
       e.preventDefault();
-      jQuery.ajax({url:href,dataType:'html'}).done(
-        function(data){
-          jQuery('#container').html(data);
-          //centerVideos();
-          bindLinksToLoadNewSections();
-        });
-    }
+      if(fade !== null && ("fadeout" == fade.toLowerCase() || "fadeinout" == fade.toLowerCase())){
+		if(speed == null){ speed = 500; }
+		jQuery('#container').animate({'opacity': 0}, speed, function(){
+			goToNextSection(href, fade, speed);
+		});
+	  } else {
+		goToNextSection(href, fade);
+	}
+	  }	
     return false;
   });
+}
+
+function goToNextSection(href, fade, speed){
+	jQuery.ajax({url:href,dataType:'html'}).done(
+		function(data){
+			jQuery('#container').html(data);
+			if(fade !== null && ("fadein" == fade.toLowerCase() || "fadeinout" == fade.toLowerCase())){
+				jQuery('#container').animate({'opacity':1}, speed, function(){
+					bindLinksToLoadNewSections();
+				});
+			} else{
+				jQuery('#container').css({'opacity':1});
+				bindLinksToLoadNewSections();
+			}
+			centerVideos();
+		});
 }
 
 function centerVideos(){
@@ -42,13 +62,27 @@ function centerVideos(){
 	$(window).on('resize', function(){
 		if($(window).width() * videoheight/videowidth > $(window).height){
 			$('#container').css({'width':Math.floor($(window).height() * videowidth/videoheight), 'height':$(window).height, 'left':($(window).width() - videowidth)/2});
+			$('.nav').css({'top': $('#container').css('top')});
 		} else {
 			$('#container').css({'width':'100%', 'height': Math.floor($(window).width() * videoheight/videowidth), 'top':($(window).height() - Math.floor($(window).width() * videoheight/videowidth))/2});
+			$('.nav').css({'top': $('#container').css('top')});
 		}
 	})
+	
+	$('.nav').css({'top': $('#container').css('top')});
 }
 
-
+/**
+  *Fade transitions. Use the data-transition attribute on the a tags.
+  *
+  *fadeInOut = Fade current slide out, and next slide in.
+  *fadeIn = Fade next slide in, but current slide disappears immediately
+  *fadeOut = Current slide fades out, next slide cuts in immediately
+  *none (or no attribute/blank) = straight cuts.
+  */
+ function fadeTransition(type){
+	
+}
 
 jQuery(document).ready(function($){
   
